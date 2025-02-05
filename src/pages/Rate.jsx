6d0acf1e7e4fa53e018f82d9fe4ef23247c2f7a8
Will Palmer <wop3000@hotmail.co.uk>
2025-02-05 13:47:50 +0000

    added rate card component

--- src/pages/Rate.jsx
+++ src/pages/Rate.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import activities from "../assets/activities"
+import RateCard from "../components/RateCard"
 
 function Rate() {
 
@@ -10,6 +12,11 @@ function Rate() {
             <p>
               Rate the activities
             </p>
+            <RateCard 
+                title={activities[0].title}
+                description={activities[0].description}
+                url={activities[0].image_link}
+            />
           </div>
           <Link to ="/">Home</Link>
           <Link to="/itinerary">Itinerary</Link>
